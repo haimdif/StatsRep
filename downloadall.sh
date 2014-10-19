28c713fad8e183e54db56db85dca4ce08344718b
elad dinur <edinur@leon.il.nds.com>
2014-10-19 16:35:54 +0300

    add play by play files

--- downloadall.sh
+++ downloadall.sh
@@ -1,7 +1,8 @@
-I=0
+I=1907
 while [ $I -lt 2000 ] ; do
     wget http://www.basket.co.il/misc/Stats-XML/isr-$I.xml > /dev/null 2>&1
     let I=I+1 
 done
 
 grep -lrIZ javascript . | xargs -0 rm -f
+
