747a7b6c48c4b10a3cef3296cd10725a592f5236
elad dinur <edinur@leon.il.nds.com>
2014-12-03 11:06:09 +0200

    add games

--- downloadall.sh
+++ downloadall.sh
@@ -1,4 +1,5 @@
-I=1952
+I=`ls -l | awk '{print $9}' | tail -n 1 | cut -c 5,6,7,8`
+let I=I+1 
 while [ $I -lt 2000 ] ; do
     wget http://www.basket.co.il/misc/Stats-XML/isr-$I.xml? > /dev/null 2>&1
     let I=I+1 
