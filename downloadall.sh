327edcdd183dbf61b3022e3ca389331714bf88e6
elad dinur <edinur@leon.il.nds.com>
2015-01-11 17:13:45 +0200

    add games

--- downloadall.sh
+++ downloadall.sh
@@ -1,6 +1,6 @@
 I=1906
 let I=I+1 
-while [ $I -lt 2000 ] ; do
+while [ $I -lt 2100 ] ; do
     if [ ! -f isr-$I.xml ]; then
         wget http://www.basket.co.il/misc/Stats-XML/isr-$I.xml? > /dev/null 2>&1
         echo "isr-$I.xml downloaded"
