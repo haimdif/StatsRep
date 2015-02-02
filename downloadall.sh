f99234f831e23febbd3f1df7dbb07bd52b0143df
elad dinur <edinur@leon.il.nds.com>
2015-02-02 16:16:40 +0200

    downall not working well

--- downloadall.sh
+++ downloadall.sh
@@ -2,9 +2,7 @@ I=1906
 let I=I+1 
 while [ $I -lt 2100 ] ; do
     if [ ! -f isr-$I.xml ]; then
-        echo "http://www.basket.co.il/misc/Stats-XML/isr-$I.xml?"
-        exit
-        wget http://www.basket.co.il/misc/Stats-XML/isr-$I.xml? > /dev/null 2>&1
+        wget http://www.basket.co.il/misc/Stats-XML/isr-$I.xml > /dev/null 2>&1
         echo "isr-$I.xml downloaded"
     fi
     let I=I+1 
